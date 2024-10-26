fc4bb6f7c0bac70d8674a3d82efb0b713deae263
Ebenezer <eebenezer949@gmail.com>
2024-10-26 18:47:34 +0100

    feat: add move valueobject to game domain

---
+++ internal/core/domain/move.go
@@ -0,0 +1,27 @@
+package domain
+
+// Move is a value-object that represents a move a player can make
+// 
+// Rock == 0
+//
+// Paper == 1
+// 
+// Scissor == 2
+//
+// Any other Move is considered invalid, these outcomes have a string representation
+type Move int
+
+const (
+	Rock Move = iota
+	Paper
+	Scissor
+)
+
+func (m Move) String() string {
+	return [...]string{"Rock", "Paper", "Scissor"}[m]
+}
+
+// IsValid checks if a move is valid, i.e one of Rock, Paper, Scissors
+func (m Move) IsValid() bool {
+	return m == Rock || m == Paper || m == Scissor
+}
