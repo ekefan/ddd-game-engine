8a68605f4fa42cc730b8d70ad43da627686f904e
Ebenezer <eebenezer949@gmail.com>
2024-10-26 18:46:04 +0100

    test: adapter for session in memory storage

---
+++ internal/adapters/memory/session-memory_test.go
@@ -0,0 +1,139 @@
+package memory
+
+import (
+	"testing"
+
+	"github.com/ekefan/ddd-game-engine/internal/core/domain/session"
+	session_repo "github.com/ekefan/ddd-game-engine/internal/ports/repository"
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func newSession(t *testing.T) session.Session {
+	ns, err := session.NewSession(
+		session.WithPlayer1(&websocket.Conn{}, "testPlayer1"),
+		session.WithPlayer2(&websocket.Conn{}, "testPlayer2"),
+	)
+	require.NoError(t, err)
+	require.NotEmpty(t, ns)
+	return ns
+}
+
+func TestGet(t *testing.T) {
+	type testCase struct {
+		test        string
+		id          uuid.UUID
+		expectedErr error
+	}
+
+	ns := newSession(t)
+	id := ns.GetID()
+	repo := NewSessionRepository()
+	repo.sessions[id] = ns
+	testCases := []testCase{
+		{
+			test:        "session exists",
+			id:          id,
+			expectedErr: nil,
+		}, {
+			test:        "session doesn't exist",
+			id:          uuid.MustParse("f47ac10b-58cc-0372-8567-0e02b2c3d479"),
+			expectedErr: session_repo.ErrSessionNotFound,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.test, func(t *testing.T) {
+			_, err := repo.Get(tc.id)
+			assert.Equal(t, tc.expectedErr, err)
+		})
+	}
+}
+
+func TestAdd(t *testing.T) {
+	type testCase struct {
+		test        string
+		sess        session.Session
+		expectedErr error
+	}
+	ns := newSession(t)
+	sessions := NewSessionRepository()
+	sessions.sessions[ns.GetID()] = ns
+	testCases := []testCase{
+		{
+			test:        "session already exists",
+			sess:        ns,
+			expectedErr: session_repo.ErrFailedToAddSession,
+		}, {
+			test:        "session doesn't exist",
+			sess:        newSession(t),
+			expectedErr: nil,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.test, func(t *testing.T) {
+			err := sessions.Add(tc.sess)
+			assert.Equal(t, tc.expectedErr, err)
+		})
+	}
+}
+func TestUpdate(t *testing.T) {
+	type testCase struct {
+		test        string
+		sess        session.Session
+		expectedErr error
+	}
+	ns := newSession(t)
+	sessions := NewSessionRepository()
+	sessions.sessions[ns.GetID()] = ns
+	testCases := []testCase{
+		{
+			test:        "session doesn't exist",
+			sess:        newSession(t),
+			expectedErr: session_repo.ErrFailedToUpdateSession,
+		}, {
+			test:        "session exist",
+			sess:        ns,
+			expectedErr: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.test, func(t *testing.T) {
+			err := sessions.Update(tc.sess)
+			assert.Equal(t, tc.expectedErr, err)
+		})
+	}
+}
+func TestDelete(t *testing.T) {
+	type testCase struct {
+		test        string
+		id          uuid.UUID
+		expectedErr error
+	}
+
+	ns := newSession(t)
+	id := ns.GetID()
+	repo := NewSessionRepository()
+	repo.sessions[id] = ns
+	testCases := []testCase{
+		{
+			test:        "session exists",
+			id:          id,
+			expectedErr: nil,
+		}, {
+			test:        "session doesn't exist",
+			id:          uuid.MustParse("f47ac10b-58cc-0372-8567-0e02b2c3d479"),
+			expectedErr: session_repo.ErrFailedToDeleteSession,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.test, func(t *testing.T) {
+			err := repo.Delete(tc.id)
+			assert.Equal(t, tc.expectedErr, err)
+		})
+	}
+}
