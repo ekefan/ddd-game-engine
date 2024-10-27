a054832f670ce5546aeaedb6c2383a1bd4758ddf
Ebenezer <eebenezer949@gmail.com>
2024-10-27 12:03:26 +0100

    refactor

--- internal/ports/api/game.go
+++ internal/ports/api/game.go
@@ -1,6 +1,13 @@
 package ports
 
+import (
+	"github.com/ekefan/ddd-game-engine/internal/core/domain"
+	"github.com/ekefan/ddd-game-engine/internal/core/domain/session"
+	"github.com/google/uuid"
+)
+
 type GameService interface {
-	WaitInLobby() error //start route post
-	PlayGame() error    //play route web socket
+	CreateSession(player *domain.Player) (*session.Session, error)
+	GetSession(id uuid.UUID) (*session.Session, error)
+	PlayGame(id uuid.UUID, player2 *domain.Player) error //play route web socket
 }
