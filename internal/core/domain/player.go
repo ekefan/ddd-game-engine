241688129ba4706cb22a3596252281fafcf114e6
Ebenezer <eebenezer949@gmail.com>
2024-10-27 12:00:58 +0100

    feat: read players move and write outcome to them

--- internal/core/domain/player.go
+++ internal/core/domain/player.go
@@ -4,12 +4,16 @@ import (
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 )
+const (
+	DefaultPlayer1Name = "player1"
+	DefaultPlayer2Name = "player2"
+)
 
 // Player is an entity that represents a player in all sub-domains
 type Player struct {
 	ID         uuid.UUID
 	Name       string
-	Connection *websocket.Conn
 	Move       Move
+	Connection *websocket.Conn
 	Points     int16
 }
