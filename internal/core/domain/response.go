9bc978a5d273797437792fdf02571e09f5a6f108
Ebenezer <eebenezer949@gmail.com>
2024-10-28 00:04:46 +0100

    refactor response

--- internal/core/domain/response.go
+++ internal/core/domain/response.go
@@ -3,5 +3,6 @@ package domain
 type Response struct {
 	Round        int    `json:"round"`
 	Winner       string `json:"winner"`
+	RoundOutcome string`json:"round_outcome"`
 	SessionEnded bool   `json:"session_ended"`
 }
