51384f562ae2ab8a08df865c27984f9b2ef89475
Ebenezer <eebenezer949@gmail.com>
2024-10-27 11:53:59 +0100

    feat: add player move to hold move and conn

--- internal/core/domain/move.go
+++ internal/core/domain/move.go
@@ -1,11 +1,13 @@
 package domain
 
+import "github.com/gorilla/websocket"
+
 // Move is a value-object that represents a move a player can make
-// 
+//
 // Rock == 0
 //
 // Paper == 1
-// 
+//
 // Scissor == 2
 //
 // Any other Move is considered invalid, these outcomes have a string representation
@@ -24,4 +26,9 @@ func (m Move) String() string {
 // IsValid checks if a move is valid, i.e one of Rock, Paper, Scissors
 func (m Move) IsValid() bool {
 	return m == Rock || m == Paper || m == Scissor
+}
+
+type PlayerMove struct {
+	Move Move
+	Conn *websocket.Conn
 }
