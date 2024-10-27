71d8f06161911ce4cef0140178a42ada2f37d6cd
Ebenezer <eebenezer949@gmail.com>
2024-10-27 12:14:24 +0100

    refactor code

--- internal/ports/repository/session-repo.go
+++ internal/ports/repository/session-repo.go
@@ -3,20 +3,20 @@ package repository
 import (
 	"errors"
 
-	"github.com/google/uuid"
 	"github.com/ekefan/ddd-game-engine/internal/core/domain/session"
+	"github.com/google/uuid"
 )
 
 var (
 	ErrSessionNotFound       = errors.New("session was not found")
-	ErrFailedToAddSession    = errors.New("failed to add session")
+	ErrFailedToCreateSession = errors.New("failed to add session")
 	ErrFailedToUpdateSession = errors.New("failed to update session")
 	ErrFailedToDeleteSession = errors.New("failed to delete session")
 )
 
 type SessionRepository interface {
-	Get(id uuid.UUID) (session.Session, error)
-	Add(s session.Session) error
-	Update(s session.Session) error
-	Delete(id uuid.UUID) error
+	GetSession(id uuid.UUID) (*session.Session, error)
+	CreateSession(s *session.Session) error
+	UpdateSession(s *session.Session) error
+	DeleteSession(id uuid.UUID) error
 }
